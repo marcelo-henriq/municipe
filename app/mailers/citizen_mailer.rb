fbc229089c80b0a213cf5ac4af9f962b211f595b
marcelo-henrique-sousa <henrique19.marcelo@gmail.com>
2021-07-22 23:15:12 -0300

    fix(users): fixing devise views issues

--- app/mailers/citizen_mailer.rb
+++ app/mailers/citizen_mailer.rb
@@ -1,14 +1,14 @@
 class CitizenMailer < ApplicationMailer
   def notify_new_citizen(citizen_id)
     @citizen = Citizen.find(citizen_id)
-    return unless @citizen.present?
+    return if @citizen.blank?
 
     mail(to: [@citizen.email], subject: 'Novo cidadão incluido no sistema!' )
   end
 
   def notify_update_citizen(citizen_id)
     @citizen = Citizen.find(citizen_id)
-    return unless @citizen.present?
+    return if @citizen.blank?
 
     mail(to: [@citizen.email], subject: "Cidadão #{@citizen.name} atualizado no sistema!" )
   end
